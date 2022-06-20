48a238bcee8ea7615726212e1616567ca56fbf7f
benzkittisak <benzkittisak@stv.ac.th>
2022-06-20 12:55:11 +0700

    Initial Commit

---
+++ Challenge-AutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge-AutoLayout
+//
+//  Created by Kittisak Panluea on 20/6/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
